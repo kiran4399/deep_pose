44e317ccf33fabadb675c2aa5cd808372771f61b
kiran4399 <klekkala@usc.edu>
2017-11-26 16:13:46 -0800

    scrap removed

--- siamese.py
+++ siamese.py
@@ -2,11 +2,12 @@ import argparse
 import os
 import shutil
 import time
-
+import torch.nn.functional as F
+from torch.autograd import Variable
 import torch
 import torch.nn as nn
 import torch.nn.parallel
-import torch.backends.cudnn as cudnn
+#import torch.backends.cudnn as cudnn
 import torch.optim
 import torch.utils.data
 import torchvision.transforms as transforms
@@ -20,41 +21,71 @@ model_names = sorted(name for name in models.__dict__
 model_names.append('siamnet')
 
 
+class SPPLayer(nn.Module):
+
+    def __init__(self, num_levels, pool_type='max_pool'):
+        super(SPPLayer, self).__init__()
+
+        self.num_levels = num_levels
+        self.pool_type = pool_type
+
+    def forward(self, x):
+        bs, c, h, w = x.size()
+        pooling_layers = []
+        for i in range(self.num_levels):
+            kernel_size = int(math.ceil(h/(i+1)))
+            kernel_stride = int(math.floor(h/(i+1)))
+            if self.pool_type == 'max_pool':
+                tensor = F.max_pool2d(x, kernel_size=kernel_size,
+                                      stride=kernel_stride).view(bs, -1)
+            else:
+                tensor = F.avg_pool2d(x, kernel_size=kernel_size,
+                                      stride=kernel_stride).view(bs, -1)
+            pooling_layers.append(tensor)
+        x = torch.cat(pooling_layers, dim=-1)
+        return x
+
 class SiameseNetwork(nn.Module):
     def __init__(self):
         super(SiameseNetwork, self).__init__()
-        self.cnn1 = nn.Sequential(
-            nn.ReflectionPad2d(1),
-            nn.Conv2d(1, 4, kernel_size=3),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm2d(4),
-            nn.Dropout2d(p=.2),
-            
-            nn.ReflectionPad2d(1),
-            nn.Conv2d(4, 8, kernel_size=3),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm2d(8),
-            nn.Dropout2d(p=.2),
 
-            nn.ReflectionPad2d(1),
-            nn.Conv2d(8, 8, kernel_size=3),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm2d(8),
-            nn.Dropout2d(p=.2),
+
+        def conv_bn(inp, oup, stride):
+            return nn.Sequential(
+                nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
+                nn.BatchNorm2d(oup),
+                nn.ReLU(inplace=True)
+            )
+
+        def conv_dw(inp, oup, stride):
+            return nn.Sequential(
+                nn.Conv2d(inp, inp, 3, stride, 1, groups=inp, bias=False),
+                nn.BatchNorm2d(inp),
+                nn.ReLU(inplace=True),
+    
+                nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(oup),
+                nn.ReLU(inplace=True),
+            )
+
+        self.model = nn.Sequential(
+            conv_bn(  3,  16, 2), 
+            conv_dw( 16,  32, 1),
+            conv_dw( 32, 64, 2),
+            conv_dw(64, 64, 1),
+            conv_dw(64, 128, 2),
+            conv_dw(128, 128, 1),
+            conv_dw(128, 256, 2),
+            conv_dw(256, 512, 2),
+            conv_dw(512, 512, 1),
+            #nn.AvgPool2d(7),
         )
 
-        self.fc1 = nn.Sequential(
-            nn.Linear(8*100*100, 500),
-            nn.ReLU(inplace=True),
-
-            nn.Linear(500, 500),
-            nn.ReLU(inplace=True),
-
-            nn.Linear(500, 5)
-        )
+        self.spp = SPPLayer(4)
+        self.fc = nn.Linear(32256, 7)
 
     def forward_once(self, x):
-        output = self.cnn1(x)
+        output = self.model(x)
         output = output.view(output.size()[0], -1)
         output = self.fc1(output)
         return output
--- train.py
+++ train.py
@@ -29,23 +29,27 @@ model_names = sorted(name for name in models.__dict__
 parser = argparse.ArgumentParser(description='PyTorch Places365 Training')
 parser.add_argument('data', metavar='DIR',
                     help='path to dataset')
+parser.add_argument('traincsv', metavar='FILE',
+                    help='path to csv file for training')
+parser.add_argument('valcsv', metavar='FILE',
+                    help='path to csv file for validation')
 parser.add_argument('--arch', '-a', metavar='ARCH', default='resnet18',
                     help='model architecture: ' +
                         ' | '.join(model_names) +
                         ' (default: resnet18)')
 parser.add_argument('-j', '--workers', default=6, type=int, metavar='N',
                     help='number of data loading workers (default: 4)')
-parser.add_argument('--epochs', default=90, type=int, metavar='N',
+parser.add_argument('--epochs', default=20, type=int, metavar='N',
                     help='number of total epochs to run')
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
 parser.add_argument('-b', '--batch-size', default=256, type=int,
                     metavar='N', help='mini-batch size (default: 256)')
-parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
+parser.add_argument('--lr', '--learning-rate', default=1e-4, type=float,
                     metavar='LR', help='initial learning rate')
 parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
                     help='momentum')
-parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
+parser.add_argument('--weight-decay', '--wd', default=1e-5, type=float,
                     metavar='W', help='weight decay (default: 1e-4)')
 parser.add_argument('--print-freq', '-p', default=10, type=int,
                     metavar='N', help='print frequency (default: 10)')
@@ -57,7 +61,6 @@ parser.add_argument('--pretrained', dest='pretrained', action='store_false',
                     help='use pre-trained model')
 parser.add_argument('--num_classes',default=365, type=int, help='num of class in the model')
 parser.add_argument('--dataset',default='places365',help='which dataset to train')
-
 best_prec1 = 0
 
 
@@ -69,7 +72,7 @@ def main():
     print("=> creating model '{}'".format(args.arch))
     print("=> creating model '{}'".format(args.arch))
     if args.arch.startswith('siamese'):
-        model = tes.SiameseNetwork()
+        model = siamese.SiameseNetwork()
         print(model)
     model = torch.nn.DataParallel(model).cuda()
     # optionally resume from a checkpoint
@@ -113,7 +116,7 @@ def main():
         num_workers=args.workers, pin_memory=True)
 
     # define loss function (criterion) and pptimizer
-    criterion =Loss()
+    criterion = PLoss()
 
     optimizer = torch.optim.Adam(model.parameters(), args.lr,
                                 momentum=args.momentum,
